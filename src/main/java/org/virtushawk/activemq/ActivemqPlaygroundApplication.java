fc694674eb7737b85b12a54cff18ee4b4fb7e5ea
Roman <roma.bruhanchik@gmail.com>
2023-04-12 00:29:23 +0400

    init

---
+++ src/main/java/org/virtushawk/activemq/ActivemqPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package org.virtushawk.activemq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivemqPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivemqPlaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/virtushawk/activemq/ActivemqPlaygroundApplicationTests.java
@@ -0,0 +1,20 @@
+package org.virtushawk.activemq;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootTest
+class ActivemqPlaygroundApplicationTests {
+
+	@Autowired
+	private ApplicationContext applicationContext;
+
+	@Test
+	void contextLoads() {
+		Assertions.assertNotNull(applicationContext);
+	}
+
+}
